52f9816175c6ff480d5a9037862fbbef631458f8
Prasenjit Haty <prasenjit.haty@gmail.com>
2012-10-29 11:25:27 -0400

    Clean up
    changed function names to maintain project wide standards.

--- vahrehvah_curl.php
+++ vahrehvah_curl.php
@@ -1,5 +1,5 @@
 <?php
-include_once('curl.php');
+include_once('includes/curl.php');
 include_once('includes/logging.inc');
 include_once('includes/db_class.php');
 include_once('includes/functions.inc');
@@ -7,18 +7,18 @@ include_once('includes/functions.inc');
 set_time_limit(360000);
 
 $url = 'http://www.vahrehvah.com/';
-$main_content = getContent($url);
+$main_content = get_content($url);
 $node_results = array();
 
 
 $recipe_types = "";
-$recipe_types = extractContent('%<li><a href="indianrecipes.php" class="dir".*?</ul>.*?</li>%sim', $main_content, 0);
+$recipe_types = extract_content('%<li><a href="indianrecipes.php" class="dir".*?</ul>.*?</li>%sim', $main_content, 0);
 $recipe_types = preg_replace('/<li><a href="indianrecipes.php".*?<ul>/sim', '', $recipe_types);
 $recipe_types = preg_replace('%</ul>.*%sim', '', $recipe_types);
 
-$recipe_cat_urls = extractContentAll('/href=".+?"/sim', $recipe_types, 0);
+$recipe_cat_urls = extract_content_all('/href=".+?"/sim', $recipe_types, 0);
 
-$recipe_cat_titles = extractContentAll('%">.+?</a>%sim', $recipe_types, 0);
+$recipe_cat_titles = extract_content_all('%">.+?</a>%sim', $recipe_types, 0);
 $recipe_cat_titles = preg_replace('/">/sim', '', $recipe_cat_titles);
 $recipe_cat_titles = preg_replace('%</a>%sim', '', $recipe_cat_titles);
 p($recipe_cat_urls);
@@ -31,10 +31,10 @@ foreach ($recipe_cat_urls as $key => $recipe_cat_url) {
     $recipe_cat_url_final = preg_replace('/href="/sim', 'http://www.vahrehvah.com/', $recipe_cat_url);
     $recipe_cat_url_final = preg_replace('/"/sim', '', $recipe_cat_url_final);
 
-    $recipe_cat_content = getContent($recipe_cat_url_final);
-    $recipe_cat_content = extractContent('%<table align="center" width="730" border="0" >.*?</table>%sim', $recipe_cat_content, 0);
+    $recipe_cat_content = get_content($recipe_cat_url_final);
+    $recipe_cat_content = extract_content('%<table align="center" width="730" border="0" >.*?</table>%sim', $recipe_cat_content, 0);
 
-    $single_recipe_title_url = extractContentAll('%<a href=".*?</a>%sim', $recipe_cat_content, 0);
+    $single_recipe_title_url = extract_content_all('%<a href=".*?</a>%sim', $recipe_cat_content, 0);
     $inner_index = 0;
     foreach ($single_recipe_title_url as $key => $single_recipe_url) {
         $inner_index++;
@@ -50,28 +50,28 @@ p($node_results);
 
 
 function  getVahRehVahRecipeNode($single_recipe_url, $category) {
-    $single_recipe_url = extractContentAll('/\b(?:(?:http?):\/\/|www\.)[-A-Z0-9+&@#\/%=~_|$?!:,.]*[A-Z0-9+&@#\/%=~_|$]/sim', $single_recipe_url, 0);
+    $single_recipe_url = extract_content_all('/\b(?:(?:http?):\/\/|www\.)[-A-Z0-9+&@#\/%=~_|$?!:,.]*[A-Z0-9+&@#\/%=~_|$]/sim', $single_recipe_url, 0);
     $single_recipe_url = $single_recipe_url[0];
 
-    $recipe_content = getContent($single_recipe_url);
-    $recipe_details = extractContentAll('%<fieldset id="Category - Parent Category_fieldset" class="admin_fieldset" style="text-align:center;width:560px;" record_number="34">.*?</fieldset>%sim', $recipe_content, 0);
+    $recipe_content = get_content($single_recipe_url);
+    $recipe_details = extract_content_all('%<fieldset id="Category - Parent Category_fieldset" class="admin_fieldset" style="text-align:center;width:560px;" record_number="34">.*?</fieldset>%sim', $recipe_content, 0);
     $recipe_metadata = $recipe_details[0];
     $recipe_details = $recipe_details[1];
 
-    $recipe_title = extractContent('%<h1[^>]*>(.*?)</h1>%sim', $recipe_metadata, 1);
+    $recipe_title = extract_content('%<h1[^>]*>(.*?)</h1>%sim', $recipe_metadata, 1);
         
     $recipe_details_filtered = preg_replace('%<tr class="ingredient"[^>]*>(.*?)</tr>%sim', '', $recipe_details);
-    $ingredients = extractContentAll('%<td align="left"  style="width:[^>]*>(.*?)<br />%sim', $recipe_details_filtered, 1);
+    $ingredients = extract_content_all('%<td align="left"  style="width:[^>]*>(.*?)<br />%sim', $recipe_details_filtered, 1);
     $ingredients = array_chunk($ingredients, 3, TRUE);
 
-    $recipe_instructions = extractContent('%<td[^>]*class="instructions">(.*?)</td>%sim', $recipe_details_filtered, 1);
+    $recipe_instructions = extract_content('%<td[^>]*class="instructions">(.*?)</td>%sim', $recipe_details_filtered, 1);
     $recipe_instructions = preg_replace('%<br />%sim', '', $recipe_instructions);
 
-    $recipe_metadata_filtered = extractContent('%<table cellpadding="2"[^>]*>(.*?)</table>%sim', $recipe_metadata, 0);
+    $recipe_metadata_filtered = extract_content('%<table cellpadding="2"[^>]*>(.*?)</table>%sim', $recipe_metadata, 0);
     $recipe_metadata_filtered = preg_replace('%<table[^>]*>(.*?)</span></td>.+?</tr>%sim', '', $recipe_metadata_filtered);
     $recipe_final_metadata = get_prep_cook_time($recipe_metadata_filtered);
 
-    $recipe_summary = extractContent('%<span itemprop="summary"[^>]*>(.*?)</span>%sim', $recipe_metadata, 1);
+    $recipe_summary = extract_content('%<span itemprop="summary"[^>]*>(.*?)</span>%sim', $recipe_metadata, 1);
 
     $node = new stdClass();
     $node->category = $category;
@@ -140,19 +140,19 @@ function get_recipe_meta_value($subject) {
   return $result;
 }
 
-function extractContentAll($reg_exp, $content, $index) {
+function extract_content_all($reg_exp, $content, $index) {
   preg_match_all($reg_exp, $content, $extracted_content, PREG_PATTERN_ORDER);
   
   return $extracted_content[$index];
 }
 
-function extractContent($reg_exp, $content, $extracted_content, $flag = PREG_PATTERN_ORDER) {
+function extract_content($reg_exp, $content, $extracted_content, $flag = PREG_PATTERN_ORDER) {
   preg_match_all($reg_exp, $content, $extracted_content, $flag);
   
   return $extracted_content;
 }
 
-function getContent($url) {
+function get_content($url) {
   
   $content = get_db_curl_content($url);
   return $content;
